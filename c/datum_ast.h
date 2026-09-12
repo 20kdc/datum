40c18b59139eacd4bf59bbc777daba46476deecb
t20kdc <asdd2808@gmail.com>
2026-09-12 13:54:31 +0100

    C: Sketch out API structure going forward

---
+++ c/datum_ast.h
@@ -0,0 +1,32 @@
+/*
+ * datum-c - Quick to implement S-expression format
+ * Written starting in 2026 by contributors (see CREDITS.txt at repository's root)
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * A copy of the Unlicense should have been supplied as COPYING.txt in this repository. Alternatively, you can find it at <https://unlicense.org/>.
+ */
+
+/*
+ * `datum_ast.h`: datum-c 'AST API'.
+ * Scope is:
+ * * Reading/writing/storing entire trees using C realloc
+ * * High-level reference counted document model
+ * Scope is NOT:
+ * * Anything already covered by core or atom
+ */
+
+#ifndef _IT_DATUM_C_AST_H_
+
+#define _IT_DATUM_C_AST_H_
+
+#include "datum.h"
+#include "datum_atom.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ c/datum_atom.h
@@ -0,0 +1,33 @@
+/*
+ * datum-c - Quick to implement S-expression format
+ * Written starting in 2026 by contributors (see CREDITS.txt at repository's root)
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * A copy of the Unlicense should have been supplied as COPYING.txt in this repository. Alternatively, you can find it at <https://unlicense.org/>.
+ */
+
+/*
+ * `datum_atom.h`: datum-c 'atom comprehension API'.
+ * Scope is:
+ * * Atom comprehension (token + content to atom)
+ * * Atom writing (forwards string/symbol to Datum, provides internal values)
+ * Scope is NOT:
+ * * Anything already covered by core
+ * * AST
+ * * Anything that requires malloc, free
+ */
+
+#ifndef _IT_DATUM_C_ATOM_H_
+
+#define _IT_DATUM_C_ATOM_H_
+
+#include "datum.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
